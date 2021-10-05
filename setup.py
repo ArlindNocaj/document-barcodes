e569f92e5074e4992a40a0b37b61fa772f30ac88
Arlind Nocaj <arlind.nocaj@gmail.com>
2021-10-05 18:02:03 +0200

    test push

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+
 setuptools.setup(
     name="docbarcodes",
     version="1.0.1",
