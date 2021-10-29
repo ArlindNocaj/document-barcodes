1bec37589bcdb35ad22825f081e8ecc85f62c94c
Arlind Nocaj <arlind.nocaj@gmail.com>
2021-10-29 09:42:23 +0200

    fixing path bug and added --hex command to show hex values of raw barcodes in cli

--- docbarcodes/cli.py
+++ docbarcodes/cli.py
@@ -12,7 +12,7 @@ app = typer.Typer()
 
 
 @app.command()
-def extract(files: List[Path], try_harder: bool = False, possible_formats = ["PDF_417", "CODE_128", "QR_CODE", "AZTEC"], verbose: bool = typer.Option(False, "--verbose")):
+def extract(files: List[Path], try_harder: bool = False, possible_formats = ["PDF_417", "CODE_128", "QR_CODE", "AZTEC"], verbose: bool = typer.Option(False, "--verbose"), hex: bool = typer.Option(False, "--hex", help="outputs the raw barcodes as hex")):
 
     if not verbose:
         logger.remove()
@@ -22,14 +22,21 @@ def extract(files: List[Path], try_harder: bool = False, possible_formats = ["PD
         if not path.is_file():
             typer.echo(f"File does not exists: {path.name}")
 
-        raw, combined = process_document(path, try_harder=try_harder, possible_formats = possible_formats)
+        raw, combined = process_document(str(path), try_harder=try_harder, possible_formats = possible_formats)
 
         raw_dict = [b._asdict() for b in raw]
         combined_dict = [b._asdict() for b in combined]
 
         response = {"BarcodesRaw": raw_dict, "BarcodesCombined": combined_dict}
 
-        typer.echo(json.dumps(response))
+        if not hex:
+            typer.echo(json.dumps(response))
 
+        if hex:
+            for barcode in raw:
+                if isinstance(barcode.raw,str):
+                    typer.echo(barcode.raw.encode("ISO-8859-1").hex().upper())
+                else:
+                    typer.echo(barcode.raw)
 if __name__ == "__main__":
     app()
